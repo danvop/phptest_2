30235ab67ed32da2adfd4a68ae74f2f5922ef054
danvop <danvop@gmail.com>
2015-05-02 19:16:08 +1000

    test.php fixed

--- test/index.php
+++ test/index.php
@@ -1,5 +1,18 @@
 <?php
 //discription: this examle made for examle how make test with sessions.
+session_start();
+header('Content-Type: text/html;charset=utf-8'); //helps browser to understand charset
+if(!isset($_SESSION['test']) and !isset($_POST['q'])){
+	//if test runs first, var init
+	$q = 0; //current question number
+	$title = 'Пройдите тест';
+}else{
+	//create a new session variable 'test', which consists questions array
+	if($_POST['q'] != '1')
+		$_SESSION['test'][] = $_POST['answer'];
+	$q = $_POST['q'];
+	$title = $_POST['title'];
+}
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -17,8 +30,8 @@
 		<table width="100%">
 			<tr>
 				<td align="center">
-					<h1>...</h1>
-					<p><a href='/index.php'>Прервать тест и вернуться на сайт</a></p>
+					<h1><?= $title?></h1>
+					<p><a href='../index.php'>Прервать тест и вернуться на сайт</a></p>
 				</td>
 			</tr>
 		</table>
@@ -29,6 +42,16 @@
 <tr>
 	<td>
 		<!-- Область основного контента -->
+		<?php
+		// depending on the number of question, connect appropriate file with questions
+		switch($q){
+			case 0: include 'start.php'; break;
+			case 1: include 'q1.php'; break;
+			case 2: include 'q2.php'; break;
+			case 3: include 'q3.php'; break;
+			default: include 'result.php';
+		}
+		?>
 		<!-- Область основного контента -->
 	</td>
 </tr>
