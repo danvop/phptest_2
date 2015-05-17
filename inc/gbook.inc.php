78d0144d7d30e33a335f7d6d7e7a73f588625a10
danvop <danvop@gmail.com>
2015-05-17 05:38:55 +0300

    guest book lab start and db.php examle added

--- inc/gbook.inc.php
+++ inc/gbook.inc.php
@@ -1,4 +1,11 @@
 <!-- Основные настройки -->
+<?php
+define("DB_HOST",			"localhost");
+define("DB_LOGIN",		"root");
+define("DB_PASSWORD",	"");
+define("DB_NAME",			"gbook");
+$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+?>
 
 <!-- Основные настройки -->
 
