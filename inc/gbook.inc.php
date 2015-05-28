24a92cced1b19ea0b9cf42afd37bab844c5b341e
danvop <danvop@gmail.com>
2015-05-28 15:30:57 +0300

    database link added

--- inc/gbook.inc.php
+++ inc/gbook.inc.php
@@ -1,6 +1,11 @@
-<?php
 /* Основные настройки */
-
+<?php
+define('DB_HOST',       'localhost');
+define('DB_LOGIN',		'root');
+define('DB_PASSWORD',	'');
+define('DB_NAME',		'gbook');
+$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+?>
 /* Основные настройки */
 
 /* Сохранение записи в БД */
