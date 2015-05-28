9a3522ec348d1f869aef570996df8c0729784e4d
danvop <danvop@gmail.com>
2015-05-28 22:18:30 +0300

    insertion added

--- inc/gbook.inc.php
+++ inc/gbook.inc.php
@@ -1,16 +1,39 @@
-/* Основные настройки */
 <?php
+/* Основные настройки */
 define('DB_HOST',       'localhost');
 define('DB_LOGIN',		'root');
 define('DB_PASSWORD',	'');
 define('DB_NAME',		'gbook');
-$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
-?>
-/* Основные настройки */
+if($link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME)){
+ // echo mysqli_connect_errno();
+  //echo '<br>';
+  echo mysqli_connect_error();
+}
+
+function clearStr($data){
+  global $link; //need for useing in function
+  return mysqli_real_escape_string($link, trim(strip_tags($data)));
+}
 
 /* Сохранение записи в БД */
+if($_SERVER['REQUEST_METHOD']=='POST'){//check form sent or not
+  $name = clearStr($_POST['name']);
+  $email = clearStr($_POST['email']);
+  $msg = clearStr($_POST['msg']);
+  $sql = "INSERT INTO msgs(name, email, msg)
+          VALUES(?,?,?)";
+  //
+  $stmt = mysqli_prepare($link, $sql);
+  mysqli_stmt_bind_param($stmt,"sss",$name,$email, $msg);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close;
+  //mysqli_query($link, $sql) or die(mysqli_error($link));
+  mysqli_close($link);
+  header('Location: '.$_SERVER['REQUEST_URI']);
+  exit;
+}
 
-/* Сохранение записи в БД */
+/*  записи в БД */
 
 /* Удаление записи из БД */
 
