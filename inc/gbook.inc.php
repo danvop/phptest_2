8fb1745bd2455356538395a66fcbd8de5be3424b
danvop <danvop@gmail.com>
2015-05-29 14:28:59 +0300

    Add secure insertion, add deletion

--- inc/gbook.inc.php
+++ inc/gbook.inc.php
@@ -11,7 +11,7 @@ if($link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME)){
 }
 
 function clearStr($data){
-  global $link; //need for useing in function
+  global $link; //need for using in function
   return mysqli_real_escape_string($link, trim(strip_tags($data)));
 }
 
@@ -20,14 +20,20 @@ if($_SERVER['REQUEST_METHOD']=='POST'){//check form sent or not
   $name = clearStr($_POST['name']);
   $email = clearStr($_POST['email']);
   $msg = clearStr($_POST['msg']);
+//---------unsecure insertion----------------------------//
+//  $sql = "INSERT INTO msgs(name, email, msg)
+//          VALUES('$name','$email','$msg')";
+//  mysqli_query($link, $sql) or die(mysqli_error($link));
+//*---------unsecure insertion-----------------------------*//
+//
+//-----------secure insertion-------------------------------//
   $sql = "INSERT INTO msgs(name, email, msg)
           VALUES(?,?,?)";
-  //
   $stmt = mysqli_prepare($link, $sql);
-  mysqli_stmt_bind_param($stmt,"sss",$name,$email, $msg);
+  mysqli_stmt_bind_param($stmt,"sss",$name,$email,$msg);
   mysqli_stmt_execute($stmt);
   mysqli_stmt_close;
-  //mysqli_query($link, $sql) or die(mysqli_error($link));
+//*-----------secure insertion------------------------------*//
   mysqli_close($link);
   header('Location: '.$_SERVER['REQUEST_URI']);
   exit;
@@ -36,7 +42,16 @@ if($_SERVER['REQUEST_METHOD']=='POST'){//check form sent or not
 /*  записи в БД */
 
 /* Удаление записи из БД */
-
+if(isset($_GET['del'])){
+  $del = abs((int)$_GET['del']);
+  if($del){
+    $sql = "DELETE FROM msgs WHERE id=$del";
+    mysqli_query($link, $sql) or
+      die(mysqli_error($link));
+    header('Location: '.$_SERVER['SCRIPT_NAME'].'?id=gbook');//return to index.php?id=gbook
+    exit;
+  }
+}
 /* Удаление записи из БД */
 ?>
 <h3>Оставьте запись в нашей Гостевой книге</h3>
@@ -53,6 +68,31 @@ Email: <br /><input type="text" name="email" /><br />
 </form>
 <?php
 /* Вывод записей из БД */
-
-/* Вывод записей из БД */
+$sql = "SELECT id, name, email, msg,
+        UNIX_TIMESTAMP(datetime) as dt
+        FROM msgs
+        ORDER BY id DESC LIMIT 5";
+$res = mysqli_query($link, $sql) or
+  die(mysqli_error($link));
+mysqli_close($link);
+while($row = mysqli_fetch_assoc($res)){
+  $id = $row['id'];
+  $name = $row['name'];
+  $email = $row['email'];
+  $dt = date('d-m-Y H:i:s', $row['dt']);
+//  $msg = $row['msg'];
+//  $msg = nl2br($row['msg']);
+  $msg = str_replace('\r\n','<br />',$row['msg']);
+  echo <<<HTML
+  <hr>
+  <p>
+    <a href="mailto:$email">$name</a> @ $dt
+    <br>$msg
+  </p>
+  <p align='right'>
+    <a href="{$_SERVER['REQUEST_URI']}&del=$id
+    ">Удалить</a>
+  </p>
+HTML;
+}
 ?>
