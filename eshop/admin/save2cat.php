aef1045899f13bdb0f599cc96a85de0936830952
danvop <danvop@gmail.com>
2015-05-30 22:47:43 +0300

    save2cat is up

--- eshop/admin/save2cat.php
+++ eshop/admin/save2cat.php
@@ -1,5 +1,27 @@
 <?php
 	// подключение библиотек
 	require "secure/session.inc.php";
-	require "../inc/lib.inc.php";
 	require "../inc/config.inc.php";
+    require "../inc/lib.inc.php";
+	
+
+function clearStr($data){
+  global $link; //need for avaliable $link in function
+  return mysqli_real_escape_string($link,
+                              trim(strip_tags($data)));
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){//check form sent or not
+  $title = clearStr($_POST['title']);
+  $author = clearStr($_POST['author']);
+  $pubyear = abs((int)$_POST['pubyear']);
+  $price = abs((int)$_POST['price']);
+}
+//echo "$title"."$author"."$pubyear"."$price";//test
+if(!addItemToCatalog($title, $author, $pubyear, 
+                     $price)){
+  echo 'Произошла ошибка при добавлении товара 
+                                  в каталог'; }
+else{ header("Location: add2cat.php"); 
+     exit;
+}
