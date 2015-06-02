012e7d6d99b27fab14105b6e4a9847774bfb37f8
danvop <danvop@gmail.com>
2015-06-02 14:06:31 +0300

    delete from basket fixed

--- eshop/delete_from_basket.php
+++ eshop/delete_from_basket.php
@@ -2,3 +2,11 @@
 	// подключение библиотек
 	require "inc/lib.inc.php";
 	require "inc/config.inc.php";
+
+$id = clearInt($_GET["id"]);
+if($id){
+  deleteItemFromBasket($id);
+  $count--;
+}
+header("Location: basket.php");
+exit;
